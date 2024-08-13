7dff5dd7207138623af8af6bbd0ba73f7f1b6d09
Flavian <engevinflavian@gmail.com>
2024-08-13 12:34:26 +0200

    style: rework card grid view

--- memory/View/CardView.swift
+++ memory/View/CardView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
-// TODO: (Flavian) - Improves card border – Maybe use a gradient
-// TODO: (Flavian) - Find a way to notify user the successful pair
+// TODO: (Flavian) - Find to improve the way to notify visually user the successful pair
 struct CardView: View {
     let card: Card
     var canBeRevealed = true
@@ -13,16 +12,7 @@ struct CardView: View {
     var body: some View {
         ZStack {
             Image("back")
-                .resizable()
-                .scaledToFit()
-                .frame(width: cardSize, height: cardSize)
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(.frost.opacity(0.7), lineWidth: 2)
-                        .frame(width: cardSize, height: cardSize)
-                }
-                .rotationEffect(.degrees(card.rotation))
+                .cardStyle(cardSize: cardSize, rotation: card.rotation)
                 .rotation3DEffect(
                     .degrees(canBeRevealed && (card.isRevealed || card.isMatched) ? 90 : 0),
                     axis: (x: 0.0, y: 1.0, z: 0.0)
@@ -31,16 +21,7 @@ struct CardView: View {
                 .animation(card.isRevealed || card.isMatched ? .easeInOut : .easeInOut.delay(0.35), value: card.isRevealed)
 
             Image(card.imageName)
-                .resizable()
-                .scaledToFit()
-                .frame(width: cardSize, height: cardSize)
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(.frost.opacity(0.7), lineWidth: 2)
-                        .frame(width: cardSize, height: cardSize)
-                }
-                .rotationEffect(.degrees(card.rotation))
+                .cardStyle(cardSize: cardSize, rotation: card.rotation)
                 .scaleEffect(canBeRevealed && (card.isRevealed || card.isMatched) ? 1 : 0.8)
                 .rotation3DEffect(
                     .degrees(canBeRevealed && (card.isRevealed || card.isMatched) ? 0 : -90),
@@ -48,6 +29,29 @@ struct CardView: View {
                 )
                 .animation(card.isRevealed || card.isMatched ? .easeInOut.delay(0.35) : .easeInOut, value: card.isRevealed)
         }
+    }
+}
+
+extension Image {
+    func cardStyle(cardSize: CGFloat, rotation: Double) -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+            .frame(width: cardSize, height: cardSize)
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(
+                        LinearGradient(
+                            colors: [.frost, .charcoalBlue],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+                    .frame(width: cardSize, height: cardSize)
+            }
+            .rotationEffect(.degrees(rotation))
     }
 }
 
